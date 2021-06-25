d06abfde86bad4bc3aeca082af2f806b5d6cac2f
Eagar Satya <eagarsatya98@gmail.com>
2021-06-25 18:10:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Serilog;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using StackExchange.Redis;
+
+namespace RedisConsoleApp
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration;
+
+        static int Main(string[] args)
+        {
+            // Initialize serilog logger
+            Log.Logger = new LoggerConfiguration().CreateLogger();
+
+            try
+            {
+                MainAsync(args).Wait();
+                return 0;
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            // Create service collection
+            Log.Information("Creating service collection");
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            Log.Information("Building service provider");
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            try
+            {
+                Log.Information("Starting service");
+                await serviceProvider.GetService<RedisService>().Run();
+                Log.Information("Ending service");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error running service");
+                throw ex;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            services.AddSingleton<IConfigurationRoot>(Configuration);
+            services.AddTransient<RedisService>();
+
+            var redisConfig = Configuration.GetConnectionString("Redis");
+            var redisDefaultDb = Configuration.GetConnectionString("RedisDefaultDb");
+
+            Console.WriteLine($"Redis config {redisConfig}, Redis default DB {redisDefaultDb}");
+
+            var isValidRedisDb = int.TryParse(redisDefaultDb, out var redisDb);
+            if (isValidRedisDb == false)
+            {
+                Console.WriteLine("Cannot parse Redis Default DB value");
+                throw new InvalidCastException("Cannot parse Redis Default DB value");
+            }
+
+            var redis = ConnectionMultiplexer.Connect($"{redisConfig},defaultDatabase={redisDb}");
+            services.AddSingleton<IConnectionMultiplexer>(redis);
+            services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration = $"{redisConfig},defaultDatabase={redisDb}";
+                options.InstanceName = Configuration.GetConnectionString("RedisInstance");
+            });
+        }
+    }
+}
---
+++ RedisService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisConsoleApp
+{
+    public class RedisService
+    {
+
+        private readonly IDistributedCache RedisMan;
+        private string RedisKey = "Logging";
+
+        public RedisService(IDistributedCache distributedCache)
+        {
+            this.RedisMan = distributedCache;
+        }
+
+        public async Task<string> Run()
+        {
+            try
+            {
+                while (true)
+                {
+                    var delayTask = Task.Delay(10000);
+                    var isCurrentlyRunning = await RedisMan.GetStringAsync(RedisKey);
+                    if (!string.IsNullOrEmpty(isCurrentlyRunning))
+                    {
+                        Console.WriteLine("Function is currently running");
+                        return $"Function is currently running";
+                    }
+
+                    await RedisMan.SetStringAsync(RedisKey, DateTime.Now.ToString("yyyyMMdd HHmmss"), new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = new TimeSpan(48, 0, 0)
+                    });
+
+                    Console.WriteLine("Setted");
+
+                    await RedisMan.SetStringAsync(RedisKey, string.Empty);
+
+                    Console.WriteLine("Removed");
+                    await delayTask;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Inner Exception : " + e.InnerException);
+                Console.WriteLine("Inner Exception Message : " + e.InnerException.Message);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            return string.Empty;
+        }
+    }
+}
